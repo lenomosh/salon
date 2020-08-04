3b4981ad5a269f0fe547e29e7ed6515e6732808e
Lennox <omondilenox@outlook.com>
2020-08-04 19:45:13 +0300

    added users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::truncate();
+        $faker = \Faker\Factory::create();
+        \App\User::create([
+            'name'=>'John Does',
+            'email'=>'admin@admin.com',
+            'password'=>bcrypt('secret'),
+            'user_type_id'=>0
+
+        ]);
+
+        foreach(range(1,10) as $i){
+            \App\User::create([
+                'name'=> $faker->name,
+                'email'=>$faker->email,
+                'password'=>bcrypt('secret')
+                ]
+            );
+        }
+    }
+}
