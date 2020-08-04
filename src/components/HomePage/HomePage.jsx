f39e1c93486cb610035d3025f69c4f9994131182
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-05 01:17:32 +0300

    Added materialui styles in homepage and icons link in index

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './HomePage.styles.scss'
+import './material-dashboard.styles.scss'
 
 const HomePage=(props)=> {
   return (
-    <div className="home-page">
+    <div className="home-page-main">
       <h1>Home Page</h1>
     </div>
   );
