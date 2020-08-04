912f836ff01c39840386b37bc60eb60a2b0d21bc
nelly kamotho <nkamothO57@gmail.com>
2020-08-04 13:33:17 +0300

    add address form

--- src/components/ServicesPage/ServicesPage.component.jsx
+++ src/components/ServicesPage/ServicesPage.component.jsx
@@ -4,8 +4,14 @@ import './ServicesPage.styles.scss'
 const ServicesPage = () => {
     return (
         <div className="servicesPage">
-            <p>Services Page</p>
-
+            <h1>LOCATION</h1>
+            <P>Youn are required to input you current loacation so that we can locate spas around you </P>
+            <form action="location">
+                <div className="form-group">
+                    <input type="location" className="form-control" placeholder="input location"/>
+                    <a href="#" class="btn btn-purple btn-lg active" role="button">SUBMIT</a>
+                </div>
+            </form>
         </div>
     )
 }
