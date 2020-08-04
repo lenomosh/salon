30f34847b117e2d21c54d2e3d557b004a97114a0
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-04 21:36:32 +0300

    Added login-form and login-page styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './LoginPage.styles.scss'
 
 const LoginPage=(props)=> {
   return (
-    <div className="login-page">
-      <h1>Login Page</h1>
+    <div className="login-page text-white">
+      <div className="login-form">
+        <form>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+            <label>Email address</label>
+            <input type="email" className="form-control" placeholder="Enter email" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Enter password" />
+          </div>
+
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="customCheck1" />
+              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-block">Login</button>
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
+      </div>
+        
     </div>
   );
 }
