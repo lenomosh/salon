7137aa4f230d2cd9b9401b4ff06b435e54777130
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-04 17:22:52 +0300

    Added welcome body in welcome page component

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -6,6 +6,13 @@ const WelcomePage = () => {
   return (
     <div className="welcome-page">
       <WelcomeNavbar/>
+      <div className="welcome-page-body text-white">
+        <div className="banner">
+          <h1 className="animate__animated animate__bounce animate__slow animate__infinite">Welcome To Urembo Salons</h1>
+          <p className="d-lg-4">Every salon deserves the perfect tools for the job. Focus on your clients, not your admin,<br/> with features and services designed around you.</p>
+          <button type="button" class="btn btn-primary">Partner With Us</button>
+        </div>
+      </div>
     </div>
   )
 }
