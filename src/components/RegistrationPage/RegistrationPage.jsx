56b68caf25ffe2a2839c241bd5d5a41ae63b256f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-04 21:59:14 +0300

    Added registrationpage component initial structure

---
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegistrationPage=(props)=> {
+  return (
+    <div className="registration-page">
+      <h1>Registration</h1>      
+    </div>
+  );
+}
+
+export default RegistrationPage;
