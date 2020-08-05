85ea560b6564c1e0451f8b5c34497c98bc02118c
nelly kamotho <nkamothO57@gmail.com>
2020-08-05 10:29:09 +0300

    images

--- src/components/ServicesPage/ServicesPage.component.jsx
+++ src/components/ServicesPage/ServicesPage.component.jsx
@@ -9,39 +9,39 @@ const ServicesPage = () => {
             <form action="location">
                 <div className="form-group">
                     <input type="location" className="form-control" placeholder="input location"/>
-                    <a href="#" class="btn btn-purple btn-lg active" role="button" id="form">SUBMIT</a>
+                    <a href="#" class="btn btn-purple btn-lg active"  role="button" id="button">SUBMIT</a>
                 </div>
             </form>
-            <div className="Services Offered"></div>
-            <p>Select service that you are interested in.</p>
-            <div className="row">
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.gif" alt=""/>
-                    <h6>Massage</h6>
+            <div className="Services"></div>
+                <p>Select service that you are interested in.</p>
+                <div className="row">
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/hair.gif" alt=""/>
+                        <h2> Hair</h2>
+                    </div>
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/boyshair.gif" alt=""/>
+                        <h2>barber shop</h2>
+                    </div>
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/hairremoval.gif" alt=""/>
+                        <h2>Hair Removal</h2>
+                    </div>
                 </div>
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.png" alt=""/>
-                    <h6>Massage</h6>
+                <div className="row">
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/massage.gif" alt=""/>
+                        <h2>Massage</h2>
+                    </div>
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/nails.gif" alt=""/>
+                        <h2>Nail</h2>
+                    </div>
+                    <div className="col-md-4" id="hair">
+                        <img src="images/services/facial.gif" alt=""/>
+                        <h2>Face</h2>
+                    </div>
                 </div>
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.png" alt=""/>
-                    <h6>Massage</h6>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.png" alt=""/>
-                    <h6>Massage</h6>
-                </div>
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.png" alt=""/>
-                    <h6>Massage</h6>
-                </div>
-                <div className="col-md-4" id="hair">
-                    <img src="../images/massage.png" alt=""/>
-                    <h6>Massage</h6>
-                </div>
-            </div>
         </div>
     )
 }
